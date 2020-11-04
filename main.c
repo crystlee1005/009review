a2dc88fe5799b1b3efaf57e5efe9d91ec3df7e6e
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-11-04 14:26:38 +0900

    실습3
    랜덤 점수

--- main.c
+++ main.c
@@ -4,21 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
+	int i;
 	int grade[SIZE];
-	int i, sum, average;
+	int score[SIZE];
 	printf("%i명의 점수를 입력하세요: \n", SIZE);
 	
 
 	for (i=0; i<SIZE; i++)
-	{   scanf("%d", &grade[i]);
-	    sum+=grade[i];	
-	    average= sum/SIZE;
+	{   grade[i] = rand() %100;
+	    score[i]=grade[i];
 
 	}
 	for (i=0; i<SIZE; i++)
-	printf("grade[%i] = %i\n", i, grade[i]);
-	printf("the average is %d\n", average);
+	printf("score [%i] = %i\n", i, score[i]);
+	
 	
 	return 0;
 }
